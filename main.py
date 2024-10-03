3e0562fdc6ef9284f53162d14769dfed61b92418
Lobix <lobix2005@gmail.com>
2024-10-03 19:01:27 +0200

    Lobodiuk commit

--- main.py
+++ main.py
@@ -17,7 +17,16 @@ split_text = replaced_text.splitlines()
 joined_text = " ".join(split_text)
 
 # 6. Ткаченко Ілля: Використовуємо функцію title() для перетворення кожного слова на текст із великої літери
-final_text = joined_text.title()
+titled_text = joined_text.title()
+
+# 7. Лободюк Євгеній: Використовуємо функцію translate() та maketrans() в середині неї для видалення крапок з тексту
+no_dot_text = replaced_text.translate(str.maketrans("", "", "."))
+
+# 8. Лободюк Євгеній: Використовуємо функцію capitalize() для перетворення першого символа у першому слові на велику літеру
+capitalized_text = no_dot_text.capitalize()
+
+# 9. Лободюк Євгеній: Використовуємо функцію swapcase() для зміни регістру всіх літер на протилежний
+final_text = capitalized_text.swapcase()
 
 # Виводимо кінцевий результат
 print(final_text)
